2e2861e4857cfdd1fa885ccd479ec164e28f6221
Roneison Machado <roneison.machado@gmail.com>
2019-06-14 17:26:45 -0300

    Injetando um servico em outro servico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { LogService } from './shared/log.service';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { BrowserModule } from '@angular/platform-browser';
@@ -17,7 +18,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CriarCursoModule,
     CursosModule
   ],
-  providers: [CursosService],
+  providers: [LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,3 +1,4 @@
+import { LogService } from './../shared/log.service';
 import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
@@ -8,11 +9,17 @@ export class CursosService {
 
     private cursos: string[] = ['Angular 2', 'Java', 'C#', 'Ionic'];
 
+    constructor(private logService: LogService){
+
+    }
+
     getCursos(){
+        this.logService.consoleLog('Obtendo lista de cursos');
         return this.cursos;
     }
 
     addCurso(curso: string){
+        this.logService.consoleLog(`Adicionando um novo curso ${curso}`);
         this.cursos.push(curso);
         this.emitirCursoCriado.emit(curso);
         CursosService.criouNovoCurso.emit(curso);
